0e9620c046d00b52b3fc2d1376fe6d2ad4c06530
dmcclow12861 <davidmcclow@hotmail.com>
2016-10-18 21:21:23 -0700

    Update dbmanager.cpp

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -1,39 +1,35 @@
 #include "dbmanager.h"
-
+QString databasepath = "C:/Users/USERR/Desktop/Blockbusted/Blockbusted/blockbusted_db.db";
 //Constructor
 //Sets up the database connection and opens it
-DbManager::DbManager()
-{
-    blockbusted_db = QSqlDatabase::addDatabase("QSQLITE");
-    blockbusted_db.setDatabaseName("C:/Users/Josh/Desktop/BlockBusted/blockbusted_db.db");
-
-    if(!blockbusted_db.open())
-        qDebug() << "Error: Failed to connect to database.";
+DbManager::DbManager(){
+    blockbusted_db = new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE"));
+    blockbusted_db->setDatabaseName(databasepath);
+    if(blockbusted_db->open())
+        qDebug() << "Database is open...";
     else
-        qDebug() << "Connected to database.";
+        qDebug() << "Database did not open...";
 }
 
+void DbManager::dataClose(){
+    //qDebug() << blockbusted_db.connectionName();
+    blockbusted_db->close();
+    QString connection = blockbusted_db->connectionName();
+    delete blockbusted_db;
+    QSqlDatabase::removeDatabase(connection);
+    qDebug() << "Database connection removed";
 
-//Deconstructor
-//Closes the connection
-DbManager::~DbManager()
-{
-    blockbusted_db.close();
-    blockbusted_db.removeDatabase("blockbusted_db.db");
-    qDebug() << "Connection closed.";
 }
 
-
+/*
 //Adds a customer to the database
 //Takes in first and last name, address, phone number, and email address as arguments
 void DbManager::addCustomerToDB(QString firstName, QString lastName, QString address, QString city,
                                 QString state, QString zipCode, QString phoneNumber, QString emailAddress)
 {
     QSqlQuery query;
-
     query.prepare("INSERT INTO Customer (FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, EmailAddress)"
                   "VALUES (:FirstName, :LastName, :Address, :City, :State, :ZipCode, :PhoneNumber, :EmailAddress)");
-
     query.bindValue(":FirstName", firstName);
     query.bindValue(":LastName", lastName);
     query.bindValue(":Address", address);
@@ -42,11 +38,10 @@ void DbManager::addCustomerToDB(QString firstName, QString lastName, QString add
     query.bindValue(":ZipCode", zipCode);
     query.bindValue(":PhoneNumber", phoneNumber);
     query.bindValue(":EmailAddress", emailAddress);
-
     if(!query.exec())
     {
         qDebug() << "Add Person Error: "
                  << query.lastError();
     }
 }
-
+*/
